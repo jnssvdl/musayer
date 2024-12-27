018a0168ef9ea100abd1e51a416add9ba95bf18b
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2024-12-27 22:07:23 +0800

    feat: add input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,35 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, Platform, Alert } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [name, setName] = useState("");
+  const greet = () => {
+    if (!name) {
+      Alert.alert("Hello, World");
+    } else {
+      Alert.alert(`Hello, ${name}`);
+    }
+    setName("");
+  };
   return (
     <ThemedView>
       <ThemedText>Hello, World</ThemedText>
+      <Input
+        label="Name"
+        onChangeText={(text) => setName(text)}
+        value={name}
+        multiline
+      />
+      <Button onPress={greet} variant="default">
+        Greet
+      </Button>
     </ThemedView>
   );
 }
