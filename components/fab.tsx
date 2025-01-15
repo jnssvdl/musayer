643cce3b81776e0ee9494d5fd0a2754968437d09
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-01-15 19:21:36 +0800

    feat: add floating action button component

---
+++ components/fab.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ViewStyle,
+} from "react-native";
+
+interface FabProps extends TouchableOpacityProps {
+  style?: ViewStyle;
+}
+
+export default function Fab({ children, style, ...props }: FabProps) {
+  return (
+    <TouchableOpacity style={[styles.button, style]} {...props}>
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    backgroundColor: "#2196F3",
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 6,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+  },
+});
