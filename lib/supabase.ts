d96b95d98149e2fd3064c4671373dbe5bce67fab
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-01-17 03:30:14 +0800

    feat: generate types

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,3 +1,4 @@
+import { Database } from "@/types/database.types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { AppState } from "react-native";
@@ -5,7 +6,7 @@ import { AppState } from "react-native";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
